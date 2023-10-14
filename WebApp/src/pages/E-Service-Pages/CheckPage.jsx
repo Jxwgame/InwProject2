815b9f816ec47c3b44d8a02a06387ff880cac150
Jxwgame <109953079+Jxwgame@users.noreply.github.com>
2023-10-15 03:01:40 +0700

    Update

--- WebApp/src/pages/E-Service-Pages/CheckPage.jsx
+++ WebApp/src/pages/E-Service-Pages/CheckPage.jsx
@@ -1,54 +1,60 @@
 import React, { useState, useEffect } from "react";
-import '../../pagesCss/Check.css'
+import '../../pagesCss/Check.css';
 
 function CheckPage() {
-    const [data, setData] = useState([]);
+  const [text, setText] = useState('');
+  const [data, setData] = useState([]);
 
-  useEffect(() => {
-    // ใช้ useEffect เพื่อเรียก API และตั้งค่า state
-    fetch('/api/your-endpoint')
+  const fetchData = () => {
+    fetch(`http://localhost:3131/check1?text=${text}`)
       .then((response) => response.json())
-      .then((data) => {
-        setData(data);
+      .then((responseData) => {
+        if (Array.isArray(responseData.results)) {
+          setData(responseData.results);
+        } else {
+          console.error('Invalid API response:', responseData.results);
+          setData([]);
+        }
       })
       .catch((error) => {
         console.error('Error:', error);
+        setData([]);
       });
-  }, []);
+  };
 
-
-    return (
-        <div className="bodyBack">
-        <div className="bodyCheck">
-            <div className="check">
-                <label htmlFor="" style={{fontSize: '18px', marginRight: '2vh', display: 'flex', justifyContent: 'center'}}>กรอกหมายเลขบัตรประชาชน</label>
-                {/* <br />  */}
-                <input type="text" placeholder=""/>
-                <button className="btnnCheck">ตรวจสอบ</button>
-            </div>
-
+  return (
+    <div className="bodyBack">
+      <div className="bodyCheck">
+        <div className="check">
+          <label htmlFor="" style={{ fontSize: '18px', marginRight: '2vh', display: 'flex', justifyContent: 'center' }}>กรอกหมายเลขบัตรประชาชน</label>
+          <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="" />
+          <button className="btnnCheck" onClick={fetchData}>ตรวจสอบ</button>
         </div>
-            <div className="TableCheck">
-                <table>
-                    <tr>
-                        <th style={{width: '22%'}}>หมายเลขบัตรประชาชน</th>
-                        <th style={{width: '15%'}}>ประเภท</th>
-                        <th style={{width: '40%'}}>รายละเอียด</th>
-                        <th style={{width: '15%'}}>สถานะ</th>
-                    </tr>
-                    <tr>
-                        <td>1849901686165</td>
-                        <td>คำร้องทั่วไป</td>
-                        <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi accusamus ipsam, 
-                            aliquid esse nihil beatae asperiores vel! Excepturi, voluptas quam, illo ipsum a
-                            utem alias ut accusamus tempora voluptates repellat cumque culpa voluptatum? Nulla l
-                            aborum vero, at totam quia ea enim optio reprehenderit repellendus est provident numq
-                            uam eum laudantium magnam architecto.</td>
-                        <td>กำลังดำเนินการ</td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-    );
+      </div>
+      <div className="TableCheck">
+        <table>
+          <thead>
+            <tr>
+              <th style={{ width: '22%' }}>หมายเลขบัตรประชาชน</th>
+              <th style={{ width: '15%' }}>ประเภท</th>
+              <th style={{ width: '40%' }}>รายละเอียด</th>
+              <th style={{ width: '15%' }}>สถานะ</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={index}>
+                <td>{item.id}</td>
+                <td>{item.type}</td>
+                <td>{item.details}</td>
+                <td>{item.status}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
-export default CheckPage
+
+export default CheckPage;
