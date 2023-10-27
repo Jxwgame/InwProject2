4f3f8eeb13c73fe302c3c1e6a56b751123eca16d
BallThanapat <115175213+BallThanapat@users.noreply.github.com>
2023-10-27 11:53:41 +0700

    make oop
    สร้าง oop ที่ admin check tax and chack request

--- WebApp/src/AdminSide/AdminRequest.jsx
+++ WebApp/src/AdminSide/AdminRequest.jsx
@@ -1,89 +1,9 @@
-// import React, { useState, useEffect } from "react";
-// import HeadAdmin from "./HeaderAdmin";
-// import '../pagesCss/SubPageAdmin.css';
-
-// function AdminRequest(){
-//     const [data, setData] = useState([]);
-
-//     const fetchData = () => {
-//         fetch(`http://localhost:3131/adcheck1`)
-//         .then((response) => response.json())
-//         .then((responseData) => {
-//             if (Array.isArray(responseData.results)) {
-//             setData(responseData.results);
-//             } else {
-//             console.error('Invalid API response:', responseData.results);
-//             setData([]);
-//             }
-//         })
-//         .catch((error) => {
-//             console.error('Error:', error);
-//             setData([]);
-//         });
-//     };
-//     return (
-//         <div>
-//             <div>
-//                 <HeadAdmin/>
-//                 <div className="backgroundPage">
-//                     <h1 style={{color: 'black'}}>คำร้องเรียน</h1>
-//                     <div className="AdminTableCheck">
-//                     <table>
-//                         <thead>
-//                         <tr>
-//                             <th style={{width: '10%'}}>หมายเลขบัตรประชาชน</th>
-//                             <th style={{width: '10%'}}>ประเภท</th>
-//                             <th style={{width: '20%'}}>รายละเอียด</th>
-//                             <th style={{width: '10%'}}>สถานะ</th>
-//                         </tr>
-//                         </thead>
-//                         <tbody>
-//                             {data.map((item, index) => (
-//                             <tr key={index}>
-//                                 <td>{item.id}</td>
-//                                 <td>{item.type}</td>
-//                                 <td>{item.details}</td>
-//                                 <td>{item.status}
-//                                     <div className="TabBtnReq">
-//                                         <button className="AdminBtnReq" name="Accept">Accept</button>
-//                                         <button className="AdminBtnReq" name="Decline">Decline</button>
-//                                     </div>
-//                                 </td>
-//                             </tr>
-//                             ))}
-//                         </tbody>
-//                     </table>
-//                 </div>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
-// export default AdminRequest
 import React, { useState, useEffect } from "react";
 import HeadAdmin from "./HeaderAdmin";
 import '../pagesCss/SubPageAdmin.css';
 
-function AdminRequest() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetchData();
-  }, []); // Call fetchData function on component mount
-
-  const fetchData = () => {
-    fetch(`http://localhost:3131/adcheckreq`)
-      .then((response) => response.json())
-      .then((responseData) => {
-        setData(responseData.results || []); // Set data to an empty array if responseData.results is falsy
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-        setData([]); // Set data to an empty array in case of error
-      });
-  };
-
-  const handleAccept = (idr) => {
+class Acceptbutton{
+  handleAccept = (idr) => {
     // ส่งคำร้องขอไปยัง API endpoint เพื่ออัพเดท status เป็น 'accepted'
     fetch(`http://localhost:3131/updateStatusreq/${idr}`, {
         method: 'PUT',
@@ -95,14 +15,16 @@ function AdminRequest() {
       .then(response => response.json())
       .then(data => {
         fetchData();
+        window.location.reload()
       })
       .catch(error => {
         console.error('Error:', error);
+        window.location.reload()
       });
       
   };
 
-  const handleDecline = (idr) => {
+  handleDecline = (idr) => {
     // ส่งคำร้องขอไปยัง API endpoint เพื่ออัพเดท status เป็น 'declined'
     fetch(`http://localhost:3131/updateStatusreq/${idr}`, {
         method: 'PUT',
@@ -114,11 +36,71 @@ function AdminRequest() {
       .then(response => response.json())
       .then(data => {
         fetchData();
+        window.location.reload()
       })
       .catch(error => {
         console.error('Error:', error);
+        window.location.reload()
       });      
   };
+}
+
+function AdminRequest() {
+  const btn1 = new Acceptbutton()
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    fetch(`http://localhost:3131/adcheckreq`)
+      .then((response) => response.json())
+      .then((responseData) => {
+        setData(responseData.results || []);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        setData([]);
+      });
+  };
+
+  // const handleAccept = (idr) => {
+  //   // ส่งคำร้องขอไปยัง API endpoint เพื่ออัพเดท status เป็น 'accepted'
+  //   fetch(`http://localhost:3131/updateStatusreq/${idr}`, {
+  //       method: 'PUT',
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       },
+  //       body: JSON.stringify({ status: 'ดำเนินการแล้ว' })
+  //     })
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       fetchData();
+  //     })
+  //     .catch(error => {
+  //       console.error('Error:', error);
+  //     });
+      
+  // };
+
+  // const handleDecline = (idr) => {
+  //   // ส่งคำร้องขอไปยัง API endpoint เพื่ออัพเดท status เป็น 'declined'
+  //   fetch(`http://localhost:3131/updateStatusreq/${idr}`, {
+  //       method: 'PUT',
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       },
+  //       body: JSON.stringify({ status: 'ปฏิเสธคำร้องนี้' })
+  //     })
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       fetchData();
+  //     })
+  //     .catch(error => {
+  //       console.error('Error:', error);
+  //     });      
+  // };
 
   const [textarea, setTextarea]=  React.useState('')
     const handleConfirm = async(idr) => {
@@ -174,8 +156,8 @@ function AdminRequest() {
                   <td>
                     {item.status}
                     <div className="TabBtnReq">
-                      <button className="AdminBtnReq" onClick={() => handleAccept(item.idr)} name="Accept">Accept</button>
-                      <button className="AdminBtnReq" onClick={() => handleDecline(item.idr)} name="Decline">Decline</button>
+                      <button className="AdminBtnReq" onClick={() => btn1.handleAccept(item.idr)} name="Accept">Accept</button>
+                      <button className="AdminBtnReq" onClick={() => btn1.handleDecline(item.idr)} name="Decline">Decline</button>
                     </div>
                   </td>
                   <td>
--- WebApp/src/AdminSide/AdminTax.jsx
+++ WebApp/src/AdminSide/AdminTax.jsx
@@ -1,68 +1,72 @@
-// import React from "react";
-// import HeadAdmin from "./HeaderAdmin";
-
-// function AdminTax(){
-//     return (
-//         <div>
-//             <div>
-//                 <HeadAdmin/>
-//                 <div className="backgroundPage">
-//                     <h1>ภาษี</h1>
-//                     <div className="AdminTableCheck">
-//                     <table>
-//                         <tr>
-//                             <th style={{width: '10%'}}>เลขที่ใบเสร็จ</th>
-//                             <th style={{width: '10%'}}>ประเภทภาษี</th>
-//                             <th style={{width: '10%'}}>วันที่ที่ชำระเงิน</th>
-//                             <th style={{width: '15%'}}>สถานะ</th>
-//                         </tr>
-//                         <tr>
-//                             <td>132134TAAX#65465</td>
-//                             <td>สนามกีฬา</td>
-//                             <td>09/11/2566</td>
-//                             <td>ยังไม่มีสถานะ 
-//                                 <div className="TabBtnReq">
-//                                     <button className="AdminBtnReq" name="Accept">Accept</button>
-//                                     <button className="AdminBtnReq" name="Decline">Decline</button>
-//                                 </div>
-//                             </td>
-//                         </tr>
-//                     </table>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
-// export default AdminTax
 import React, { useState, useEffect } from "react";
 import HeadAdmin from "./HeaderAdmin";
 import '../pagesCss/SubPageAdmin.css';
 
+class Acceptbutton{
+  handleAccept = (id) => {
+    fetch(`http://localhost:3131/updateStatustax/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ status: 'Paid' })
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        fetchData();
+        window.location.reload()
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        window.location.reload()
+      });
+  };
+  handleDecline = (id) => {
+    fetch(`http://localhost:3131/updateStatustax/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ status: 'Unpaid' })
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        fetchData();
+        window.location.reload()
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        window.location.reload()
+      });
+  };
+}
+
 function AdminTax() {
-    const formattedDate = (dateString) => {
-        const date = new Date(dateString);
-        // Get the date components
-        const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, '0'); // Month is zero-based
-        const day = String(date.getDate()).padStart(2, '0');
-        return `${day}-${month}-${year}`;
-    }
+  const btn1 = new Acceptbutton()
+  const formattedDate = (dateString) => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${day}-${month}-${year}`;
+  }
   const [data, setData] = useState([]);
 
   useEffect(() => {
     fetchData();
-  }, []); // Call fetchData function on component mount
+  }, []);
 
   const fetchData = () => {
     fetch(`http://localhost:3131/adchecktax`)
       .then((response) => response.json())
       .then((responseData) => {
-        setData(responseData.results || []); // Set data to an empty array if responseData.results is falsy
+        setData(responseData.results || []);
       })
       .catch((error) => {
         console.error('Error:', error);
-        setData([]); // Set data to an empty array in case of error
+        setData([]);
       });
   };
 
@@ -76,41 +80,40 @@ function AdminTax() {
       .catch(error => console.error('Error:', error));
   };
 
-  const handleAccept = (id) => {
-    fetch(`http://localhost:3131/updateStatustax/${id}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ status: 'Paid' })
-      })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        fetchData();
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  };
-
-  const handleDecline = (id) => {
-    fetch(`http://localhost:3131/updateStatustax/${id}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ status: 'Unpaid' })
-      })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        fetchData();
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });      
-  };
+  // const handleAccept = (id) => {
+  //   fetch(`http://localhost:3131/updateStatustax/${id}`, {
+  //       method: 'PUT',
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       },
+  //       body: JSON.stringify({ status: 'Paid' })
+  //     })
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       console.log(data);
+  //       fetchData();
+  //     })
+  //     .catch(error => {
+  //       console.error('Error:', error);
+  //     });
+  // };
+  // const handleDecline = (id) => {
+  //   fetch(`http://localhost:3131/updateStatustax/${id}`, {
+  //       method: 'PUT',
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       },
+  //       body: JSON.stringify({ status: 'Unpaid' })
+  //     })
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       console.log(data);
+  //       fetchData();
+  //     })
+  //     .catch(error => {
+  //       console.error('Error:', error);
+  //     });      
+  // };
 
   const [textarea, setTextarea]=  React.useState('')
     const handleConfirm = async(id) => {
@@ -166,8 +169,8 @@ function AdminTax() {
                   <td>
                     {item.status}
                     <div className="TabBtnReq">
-                      <button className="AdminBtnReq" onClick={() => handleAccept(item.id)} name="Accept">Accept</button>
-                      <button className="AdminBtnReq" onClick={() => handleDecline(item.id)} name="Decline">Decline</button>
+                      <button className="AdminBtnReq" onClick={() => btn1.handleAccept(item.id)} name="Accept">Accept</button>
+                      <button className="AdminBtnReq" onClick={() => btn1.handleDecline(item.id)} name="Decline">Decline</button>
                     </div>
                   </td>
                   <td>
